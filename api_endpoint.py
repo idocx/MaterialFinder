948caf09314a86fbb781dc97d57b575aab615788
Yuxing Fei <yuxingfei24@gmail.com>
2020-07-01 14:13:12 -0700

    add api_endpoint

--- api_endpoint.py
+++ api_endpoint.py
@@ -1,2 +1,25 @@
 from fastapi import FastAPI
+from query_entry import search
 
+api = FastAPI()
+
+
+@api.get("/")
+def index_guide():
+    return {
+        "search_endpoint": {
+            "url": "/api/search",
+            "params": {
+                "query": "string"
+            }
+        }
+    }
+
+
+@api.get("/api/search")
+def search_endpoint(query: str):
+    result = search(query)
+    return {
+        "found": result is not None,
+        "compound": result or {}
+    }
