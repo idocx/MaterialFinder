68b8b8b9e5cab47b3f0a91ebc607f357a663b20a
Yuxing Fei <yuxingfei24@gmail.com>
2020-07-01 23:13:20 -0700

    change api endpoint

--- api_endpoint.py
+++ api_endpoint.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from query_entry import search
+from typing import Optional
 from elasticsearch import Elasticsearch
 
 api = FastAPI()
@@ -7,21 +8,13 @@ es = Elasticsearch()
 
 
 @api.get("/")
-def index_guide():
+def search_endpoint(q: Optional[str] = None):
+    if not q:
+        result = None
+    else:
+        result = search(q, es)
     return {
-        "search_endpoint": {
-            "url": "/api/search",
-            "params": {
-                "query": "string"
-            }
-        }
-    }
-
-
-@api.get("/api/search")
-def search_endpoint(query: str):
-    result = search(query, es)
-    return {
+        "query": q or "",
         "found": result is not None,
         "compound": result or {}
     }
